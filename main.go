f9400e367e94d2d24e08e4c6689020fccedf8374
daijun <daijun@virtaitech.com>
2021-07-02 18:00:13 +0800

    test

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 		logrus.Error("Get OutboundIP failed,err:%v", err)
 		return
 	}
-	fmt.Println("测试github7")
+	fmt.Println("测试github8")
 	var confObj = new(Config)
 	// 从ini中读取静态的配置文件
 	err = ini.MapTo(confObj, "./conf/config.ini")
